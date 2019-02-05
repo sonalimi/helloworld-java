8fbd390b569af69cfb435ded870010dd2c5c4c77
sonalimi <46189778+sonalimi@users.noreply.github.com>
2019-02-05 16:53:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,6 @@
-package com.sanumala;
-public class HelloWorld
-{   
-      public static void main(String[] args)
-      {       
-         System.out.println("Hello gist @ GitHub!");   
-      }
-} 
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
 
